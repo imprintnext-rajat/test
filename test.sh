0c617158b78a85a92df3c28b319662af2e30f14e
imprintnext-rajat <rajat@imprintnext.com>
2024-05-15 16:11:17 +0530

    st commit

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
 kiotfnbghithrbmn l rotbmhj'olf v
 am
 amdfmp,bd.fvfgg ldm;lg
+=======
+xgob,m fgbmffgdjklfmgv
+mkdfmfgv
+m
+bmfkg
+mfglglglglglglglgg
+
+>>>>>>> Stashed changes
