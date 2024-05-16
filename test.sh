a8c64471e9cdfde3df618c98ff271894b9f3b491
imprintnext-rajat <rajat@imprintnext.com>
2024-05-16 13:04:03 +0530

    edit test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,1 @@
-<<<<<<< Updated upstream
-kiotfnbghithrbmn l rotbmhj'olf v
-am
-amdfmp,bd.fvfgg ldm;lg
-=======
-xgob,m fgbmffgdjklfmgv
-mkdfmfgv
-m
-bmfkg
-mfglglglglglglglgg
-
->>>>>>> Stashed changes
+welcome test.sh
